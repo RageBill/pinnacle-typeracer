70aa33ab2a18ef139b777b0432d411135ed1f4e2
Kelvin Chu <chubillkelvin@gmail.com>
2020-06-22 13:18:38 +0800

    Refactor CreateGame.

--- client/src/component/CreateGame.js
+++ client/src/component/CreateGame.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import {socket} from "../socketConfig";
+import {Button, Container, Form, Header} from "semantic-ui-react";
 
 export const CreateGame = () => {
     const [nickName, setNickName] = useState("");
@@ -12,16 +13,21 @@ export const CreateGame = () => {
     const onNickNameInput = (e) => setNickName(e.target.value);
 
     return (
-        <div className="ui container center aligned">
-            <h1 className="text-centered">Create Game</h1>
-            <form onSubmit={onSubmit}>
-                <label htmlFor="nickName">Enter NickName:</label>
-                <input type="text" name="nickName" value={nickName} onChange={onNickNameInput} placeholder="Enter NickName" />
-                <br />
-                <button type="submit" className="ui primary button">
-                    Submit
-                </button>
-            </form>
-        </div>
+        <Container textAlign="center">
+            <Header as="h1">Create Game</Header>
+            <Container text textAlign="left">
+                <Form onSubmit={onSubmit}>
+                    <Form.Field>
+                        <label htmlFor="nickName">Enter your nickname:</label>
+                        <input type="text" name="nickName" value={nickName} onChange={onNickNameInput} placeholder="Enter your nickname here" />
+                    </Form.Field>
+                    <Container textAlign="center">
+                        <Button type="submit" primary disabled={nickName.length === 0}>
+                            Submit
+                        </Button>
+                    </Container>
+                </Form>
+            </Container>
+        </Container>
     );
 };
