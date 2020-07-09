bc7e5f62c250e2c9f5faf0f652e54dfc4db3bde6
RageBill <chubillkelvin@gmail.com>
2020-07-09 21:59:29 +0800

    Add join game by url functionality.

--- client/src/component/TypeRacer.tsx
+++ client/src/component/TypeRacer.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import {Redirect} from "react-router-dom";
+import React, {useEffect} from "react";
+import {Redirect, useParams} from "react-router-dom";
 import {CountDown} from "./CountDown";
 import {PartyLeaderButtonGroup} from "./PartyLeaderButtonGroup";
 import {socket} from "../socketConfig";
@@ -7,8 +7,8 @@ import {DisplayWordsAndUserInput} from "./DisplayWordsAndUserInput";
 import {ProgressBar} from "./ProgressBar";
 import {ScoreBoard} from "./ScoreBoard";
 import {DisplayGameCode} from "./DisplayGameCode";
-import {Container, Header} from "semantic-ui-react";
-import {Game} from "../type";
+import {Container, Dimmer, Loader} from "semantic-ui-react";
+import {Game, SocketSentEventView} from "../type";
 import {ChangeNameInput} from "./ChangeNameInput";
 
 interface Props {
@@ -16,11 +16,24 @@ interface Props {
 }
 
 export const TypeRacer = ({gameState}: Props) => {
+    const {gameId} = useParams();
     const {_id, players, words, isOpen, isOver} = gameState;
     const player = players.find((player) => player.socketId === socket.id());
 
+    useEffect(() => {
+        if (_id === "") {
+            socket.emit(SocketSentEventView.JOIN_GAME, {gameId, nickName: `Anonymous Racer ${Math.round(Math.random() * 1000)}`});
+        }
+    }, [_id]);
+
     if (_id === "") {
-        return <Redirect to="/" />;
+        return (
+            <Dimmer active>
+                <Loader active size="massive" indeterminate>
+                    Joining Game...
+                </Loader>
+            </Dimmer>
+        );
     } else {
         return player ? (
             <Container textAlign="center" style={{marginTop: "36px"}}>
@@ -35,9 +48,7 @@ export const TypeRacer = ({gameState}: Props) => {
                 <DisplayGameCode gameId={_id} />
             </Container>
         ) : (
-            <Header as="h1" color="red">
-                Player not found.
-            </Header>
+            <Redirect to="/" />
         );
     }
 };
