e64d89a3a08747f7fc85a52711d0193d7fb3188a
Kelvin Chu <chubillkelvin@gmail.com>
2020-07-09 14:31:38 +0800

    Refactor ProgressBar display.

--- client/src/component/ProgressBar.tsx
+++ client/src/component/ProgressBar.tsx
@@ -21,11 +21,25 @@ export const ProgressBar = ({players, player: myself, words}: Props) => {
         setOwnProgress(calculatePercentage(myself, words));
     }, [myself, words]);
 
+    const getProgressHeaderStyle = (progress: number) => {
+        return progress > 50
+            ? {
+                  position: "absolute",
+                  margin: 0,
+                  right: `${Math.min(100 - progress + 1, 45)}%`,
+              }
+            : {
+                  position: "absolute",
+                  margin: 0,
+                  left: `${Math.min(progress + 1, 45)}%`,
+              };
+    };
+
     return (
         <Container textAlign="left">
-            <Segment style={{padding: 6, marginTop: 12, marginBottom: 60}}>
+            <Segment style={{padding: 6, marginTop: 12, marginBottom: 60, height: 40}}>
                 <Progress percent={ownProgress} color={playerColor} attached="top" />
-                <Header as="span" color={playerColor} style={{position: "relative", left: `${ownProgress}%`, transition: "left"}}>
+                <Header as="span" color={playerColor} style={getProgressHeaderStyle(ownProgress)}>
                     {`You (${myself.nickName})`}
                     {ownRanking >= 0 && ` [# ${ownRanking + 1}]`}
                 </Header>
@@ -36,9 +50,9 @@ export const ProgressBar = ({players, player: myself, words}: Props) => {
                 const playerColor = getPlayerColor(index);
                 const playerRanking = scoreBoard.findIndex((_) => _.socketId === player.socketId);
                 return player.socketId !== myself.socketId ? (
-                    <Segment style={{padding: 6, marginBottom: 12}}>
+                    <Segment style={{padding: 6, marginBottom: 12, height: 40}}>
                         <Progress percent={playerProgress} color={playerColor} attached="top" />
-                        <Header as="span" color={playerColor} style={{position: "relative", left: `${playerProgress}%`, transition: "left"}}>
+                        <Header as="span" color={playerColor} style={getProgressHeaderStyle(playerProgress)}>
                             {player.nickName}
                             {playerRanking >= 0 && ` [# ${playerRanking + 1}]`}
                         </Header>
