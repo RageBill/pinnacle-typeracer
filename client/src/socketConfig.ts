ec43a1b5acd1563d2687e5bb4d156ff16271b58c
Kelvin Chu <chubillkelvin@gmail.com>
2020-07-10 14:08:39 +0800

    Update client socketConfig to take window.location.origin.

--- client/src/socketConfig.ts
+++ client/src/socketConfig.ts
@@ -1,15 +1,15 @@
 import io from "socket.io-client";
 import {SocketReceivedEventData, SocketReceivedEventView, SocketSentEventData, SocketSentEventView} from "./type";
 
-const socket = io("http://" + process.env.REACT_APP_IP_ADDRESS + ":3001");
+const socket = io(window.location.origin);
 
 // Use customSocket for stricter type-checking
 const customSocket = {
     id: () => socket.id, // use callback to get the most updated id
     on: <T extends SocketReceivedEventView>(event: T, fn: (data: SocketReceivedEventData[T]) => void) => socket.on(event, fn),
     emit: <T extends SocketSentEventView>(event: T, data: SocketSentEventData[T]) => socket.emit(event, data),
-    removeListener: (event: string) => socket.removeListener(event), // TODO: seems not working, need to investigate
-    removeAllListeners: () => socket.removeAllListeners(), // TODO: seems not working, need to investigate
+    removeListener: (event: string) => socket.removeListener(event),
+    removeAllListeners: () => socket.removeAllListeners(),
 };
 
 export {customSocket as socket};
