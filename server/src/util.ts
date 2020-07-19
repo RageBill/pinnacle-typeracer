500add8a360f1f00ebfabe6904daf8e60b7d1a76
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2020-07-19 09:40:10 +0800

    Refactor startGameClock to have less nesting

--- server/src/util.ts
+++ server/src/util.ts
@@ -1,6 +1,17 @@
 import {Game, PlayerProps} from "./Models/Game";
 import {IO, SocketSentEventView} from "./type";
 
+/**
+ * Similar to [globalThis.setInterval], but calls the handler function immediately instead of after initial delay.
+ * @param handler callback function to execute every [delay] milliseconds
+ * @param delay   time (in milliseconds) delay between executions of [handler]
+ * @param args    arguments passed to [handler] during execution
+ */
+function setIntervalImmediately<Args extends any[]>(handler: (...args: Args) => void, delay: number = 0, ...args: Args): NodeJS.Timeout {
+    handler(...args);
+    return setInterval(handler as (...args: any[]) => void, delay, ...args);
+}
+
 const startGameClock = (io: IO) => async (gameId: string) => {
     const game = await Game.findById(gameId);
     if (game) {
@@ -8,40 +19,33 @@ const startGameClock = (io: IO) => async (gameId: string) => {
         await game.save();
         let time = Math.ceil((game.words.length * 60) / 40); // calculate total time for the race - the baseline is 40 wpm to finish the race
 
-        const timerId = setInterval(
-            (function gameIntervalFunc() {
-                Game.findById(gameId).then((game) => {
+        const timerId = setIntervalImmediately(async () => {
+            const game = await Game.findById(gameId);
+            if (game) {
+                const formatTime = calculateTime(time);
+                if (time > 0 && !game.isOver) {
+                    io.to(gameId).emit(SocketSentEventView.TIMER, {countDown: formatTime, msg: "Time Remaining"});
+                    time--;
+                } else {
+                    clearInterval(timerId);
+                    io.to(gameId).emit(SocketSentEventView.TIMER, {countDown: formatTime, msg: "Race Ended"});
+                    const endTime = new Date().getTime();
+                    let game = await Game.findById(gameId);
                     if (game) {
-                        const formatTime = calculateTime(time);
-                        if (time > 0 && !game.isOver) {
-                            io.to(gameId).emit(SocketSentEventView.TIMER, {countDown: formatTime, msg: "Time Remaining"});
-                            time--;
-                        } else {
-                            clearInterval(timerId!);
-                            io.to(gameId).emit(SocketSentEventView.TIMER, {countDown: formatTime, msg: "Race Ended"});
-                            (async () => {
-                                const endTime = new Date().getTime();
-                                let game = await Game.findById(gameId);
-                                if (game) {
-                                    const {startTime} = game;
-                                    game.isOver = true;
-                                    game.players.forEach((player: PlayerProps, index: number) => {
-                                        // calculate WPM for all those who didn't finish the race
-                                        if (game && player.WPM === -1) {
-                                            game.players[index].WPM = calculateWPM(startTime!, endTime, player);
-                                        }
-                                    });
-                                    game = await game.save();
-                                    io.to(gameId).emit(SocketSentEventView.UPDATE_GAME, {game});
-                                }
-                            })();
-                        }
+                        const {startTime} = game;
+                        game.isOver = true;
+                        game.players.forEach((player: PlayerProps, index: number) => {
+                            // calculate WPM for all those who didn't finish the race
+                            if (game && player.WPM === -1) {
+                                game.players[index].WPM = calculateWPM(startTime!, endTime, player);
+                            }
+                        });
+                        game = await game.save();
+                        io.to(gameId).emit(SocketSentEventView.UPDATE_GAME, {game});
                     }
-                });
-                return gameIntervalFunc; // return the function for setInterval to execute
-            })(), // execute the function immediately
-            1000
-        );
+                }
+            }
+        }, 1000);
     }
 };
 
